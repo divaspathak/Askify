eb5bcad002b92468d2e71a0a36c61a6a4a33d5e3
Divas Pathak <34067518+divaspathak@users.noreply.github.com>
2025-09-21 21:45:21 +0530

    Configured Kafka

--- src/main/java/com/example/Askify/Controllers/QuestionController.java
+++ src/main/java/com/example/Askify/Controllers/QuestionController.java
@@ -1,9 +1,7 @@
 package com.example.Askify.Controllers;
 
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
